d195132dc47ebe4f24c14b7c7c9b5c24eb1c2157
Zhenlong Xu <391275002@qq.com>
2017-02-11 16:15:45 +0800

    change main

--- build/lib/rqalpha/__main__.py
+++ build/lib/rqalpha/__main__.py
@@ -291,6 +291,7 @@ def show_draw_result(title, results_df):
     ax.grid(b=True, which='major', linewidth=1)
 
     ax.plot(results_df["benchmark_total_returns"], label="benchmark", alpha=1, linewidth=2, color=blue)
+    print results_df["benchmark_total_returns"]
     ax.plot(results_df["total_returns"], label="strategy", alpha=1, linewidth=2, color=red)
 
     # manipulate
